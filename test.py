43903c62d5019de320b4cebeb1bfe29f8bdd8f90
rafayc179 <rxc570@case.edu>
2023-10-10 17:21:42 -0400

    found better data file

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import numpy as np
 from Bio.Affy import CelFile
+import csv
 
 with open ("GSM7585785_31116_HF-30512_RA_SPR856_HGU133P.CEL", "r") as handle:
     c = CelFile.read(handle)
 
-    print (c.ncols, c.nrows)
-    print(c.intensities)
-    print(c.stdevs)
-    print(c.npix)
+#print(c.ncols, c.nrows)
+#print(c.intensities)
+#print(c.stdevs)
+#print(c.npix)
+print(c.AlgorithmParameters)
+
+arr = np.arange(c.ncols, c.nrows)
+
+    
