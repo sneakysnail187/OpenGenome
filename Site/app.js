40b132cd28683cbeeca45c9da3624cfa777f22dc
sneakysnail187 <maduvivier53@gmail.com>
2023-12-01 12:53:08 -0500

    csv uploading

--- Site/app.js
+++ Site/app.js
@@ -1,5 +1,50 @@
 'use strict';
 const switcher = document.querySelector('.btn');
+const input = document.getElementById("input");
+const preview = document.querySelector(".preview");
+
+input.style.opacity = 0;
+
+input.addEventListener("change", processData, false);
+
+function processData(){
+    while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+    }
+    
+    const curFiles = input.files;
+    if (curFiles.length === 0) {
+        const para = document.createElement("p");
+        para.textContent = "No file selected";
+        preview.appendChild(para);
+    } 
+    else {
+        const list = document.createElement("ol");
+        preview.appendChild(list);
+    
+        for (const file of curFiles) {
+            const listItem = document.createElement("li");
+            const para = document.createElement("p");
+            if (file.type == "text/csv") {
+                para.textContent = `File name ${file.name}, file size ${returnFileSize(
+                file.size,
+            )}.`;
+            const image = document.createElement("img");
+            image.src = URL.createObjectURL(file);
+    
+            listItem.appendChild(image);
+            listItem.appendChild(para);
+            }
+            else {
+                para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+                listItem.appendChild(para);
+            }
+    
+            list.appendChild(listItem);
+        }
+    }
+}
+
 switcher.addEventListener('click', function(){
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
