a1b0cf4d790c3c8800f2e4468459dc50cc58319b
rafayc179 <rxc570@case.edu>
2023-12-03 20:58:00 -0500

    cleaned up files for django stuff and changed settings
    django is ready to host static files-- once frontend is fully completed can do it very fast, tested on local already with the basic site

--- Site/app.js
+++ Site/app.js
@@ -1,5 +1,5 @@
 'use strict';
-const switcher = document.querySelector('.btn');
+
 const input = document.getElementById("input");
 const preview = document.querySelector(".preview");
 
@@ -44,17 +44,17 @@ function processData(){
         }
     }
 }
-
+const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function(){
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
     const className = document.body.className;
     if(className == "light-theme") {
-        this.textContent ="Dark";
+        this.textContent ="Light";
     }
     else{
-        this.textContent = "Light";
+        this.textContent = "Dark";
     }
     
     console.log('current class name: ' + className);
