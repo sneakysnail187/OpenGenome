010ab80de8de8749cbbd7b8e9c6f4fd1203982fa
rafayc179 <rxc570@case.edu>
2023-12-04 20:52:44 -0500

    adding more
    added another url , setting up more for other pages

--- Django/polls/views.py
+++ Django/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#functions for rendering the individual html pages 
 def index(request):
     return render(request, 'index.html')
-#returns the index.html page
+
+def about(request):
+    return render(request, 'about.html')
